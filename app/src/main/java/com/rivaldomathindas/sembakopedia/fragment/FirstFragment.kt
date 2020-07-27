6c6f88eb9dd7e92c06f9e50d507662d853ea46ff
mathindas.r <mathindas.r@gmail.com>
2020-07-27 21:19:56 +0800

    add login and register

--- app/src/main/java/com/rivaldomathindas/sembakopedia/fragment/FirstFragment.kt
+++ app/src/main/java/com/rivaldomathindas/sembakopedia/fragment/FirstFragment.kt
@@ -1,7 +1,5 @@
 package com.rivaldomathindas.sembakopedia.fragment
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,11 +8,11 @@ import androidx.fragment.app.Fragment
 import com.rivaldomathindas.sembakopedia.R
 
 class FirstFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 }
